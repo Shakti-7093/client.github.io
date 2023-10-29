5722ecc1173a5ddf47b4be43b42e1f1c68a423a6
shakti7093 <145318489+Shakti-7093@users.noreply.github.com>
2023-10-29 07:18:47 +0530

    Update Contact.js

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -6,7 +6,7 @@ const Contact = ()=>{
 
     const userContect = async () => {
         try {
-          const res = await fetch('https://mern-gold.vercel.app/router/auth.js', {
+          const res = await fetch('/getdata', {
             method: "GET",
             headers: {
                 "Content-Type":"application/json"
