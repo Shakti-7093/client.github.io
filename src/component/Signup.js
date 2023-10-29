f70706f5c3be110f15da68fbf020937f2778a43c
shakti7093 <145318489+Shakti-7093@users.noreply.github.com>
2023-10-29 07:40:27 +0530

    Update Signup.js

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -20,7 +20,7 @@ const Signup = ()=>{
     const PostData = async (e) => {
         e.preventDefault();
         const { name, email, phone, work, password, confpassword } = user;
-        const res = await fetch("/register", {
+        const res = await fetch("https://eager-pig-peplum.cyclic.app/", {
             method: "POST",
             headers: {
                 "Content-Type":"application/json"
